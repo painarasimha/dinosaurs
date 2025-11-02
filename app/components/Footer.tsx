79e12e22db05704693dc02bade33ff8c997632d4
painarasimha <pai.narasimha04@gmail.com>
2025-11-02 20:43:29 +0530

    [del] Deleted previous version of the Website

--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-gray-100 text-sm py-6 px-4 text-center">
-      © {new Date().getFullYear()} YourBrand. All rights reserved.
-    </footer>
-  )
-}
